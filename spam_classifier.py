69c9e7067c904413de3f6d944b86b47ffb443ef7
him000 <38493189+him000@users.noreply.github.com>
2019-08-02 10:17:02 +0530

    Add files via upload

---
+++ spam_classifier.py
@@ -0,0 +1,36 @@
+# Import Pandas library
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score,confusion_matrix
+df = pd.read_csv('spam.csv', 
+                   sep='\t', 
+                   names=['label','sms_message'])
+
+# Conversion
+df['label'] = df.label.map({'ham':0, 'spam':1})
+
+# Print dataset shape
+print (df.shape)
+
+x=df['sms_message']
+y= df['label']
+
+
+x_train,x_test,y_train,y_test = train_test_split(df['sms_message'],df['label'])
+
+count_vector = CountVectorizer(stop_words = 'english')
+
+trainning = count_vector.fit_transform(x_train)
+testing = count_vector.transform(x_test)
+
+model = MultinomialNB()
+
+model.fit(trainning,y_train)
+
+prediction = model.predict(testing)
+
+confusion_matrix(y_test,prediction)
+print("Accuracy : ",(accuracy_score(y_test,prediction)*100))
+print("Precision : ",(precision_score(y_test,prediction)*100))
